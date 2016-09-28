cd065ea5e11e428269c854193b29c4636ad8a63d
Wiciaki <wicia_6@wp.pl>
2016-09-28 20:47:41 +0200

    Version push

--- SparkTech.SDK/Variables.cs
+++ SparkTech.SDK/Variables.cs
@@ -12,9 +12,9 @@
     [Trigger]
     public static class Variables
     {
-        public const string VersionSDK = "0.1.0.3";
+        public const string VersionSDK = "0.1.1.3";
 
-        public const string VersionAllyPingSpammer = "1.0.1.2";
+        public const string VersionAllyPingSpammer = "1.2.1.2";
 
         public const string RawVariablesPath = "https://raw.githubusercontent.com/Wiciaki/EloBuddy/master/SparkTech.SDK/Variables.cs";
 
