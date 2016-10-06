385609cfb25619e621db4c931e3515ed208d725e
Wiciaki <wicia_6@wp.pl>
2016-10-06 15:48:18 +0200

    push

--- SparkTech.SDK/Variables.cs
+++ SparkTech.SDK/Variables.cs
@@ -9,24 +9,61 @@
     using SparkTech.SDK.Executors;
     using SparkTech.SDK.Web;
 
+    /// <summary>
+    /// The delegate used for passing argumentless Boolean pointers
+    /// </summary>
+    /// <returns></returns>
+    public delegate bool Predicate();
+
+    /// <summary>
+    /// The main event delegate used for handling most of the event data instances
+    /// </summary>
+    /// <typeparam name="TEventArgs">The destination event args</typeparam>
+    /// <param name="args">The event data</param>
+    public delegate void EventDataHandler<in TEventArgs>(TEventArgs args) where TEventArgs : EventArgs;
+
+    /// <summary>
+    /// The variable storage and menu initializer
+    /// </summary>
     [Trigger]
     public static class Variables
     {
+        /// <summary>
+        /// The version for the current assembly
+        /// </summary>
         public const string VersionSDK = "0.1.1.3";
 
+        /// <summary>
+        /// The version for the <see cref="E:AllyPingSpammer"/>
+        /// </summary>
         public const string VersionAllyPingSpammer = "1.2.1.2";
 
+        /// <summary>
+        /// The path to web version of the current class
+        /// </summary>
         public const string RawVariablesPath = "https://raw.githubusercontent.com/Wiciaki/EloBuddy/master/SparkTech.SDK/Variables.cs";
 
+        /// <summary>
+        /// Determines whether this run is a first one in the specified environment
+        /// </summary>
         public static readonly bool FirstRun;
 
+        /// <summary>
+        /// Gets the currently executing assembly
+        /// </summary>
         public static Assembly Assembly => Bootstrap.Assembly;
 
+        /// <summary>
+        /// The SDK menu
+        /// </summary>
         public static readonly Menu SDKMenu;
 
+        /// <summary>
+        /// Initializes static members of the <see cref="Variables"/> class
+        /// </summary>
         static Variables()
         {
-            SDKMenu = MainMenu.AddMenu("SparkTech.SDK", "st.sdk", "Spark's Software Development Kit");
+            SDKMenu = MainMenu.AddMenu("SparkTech.SDK", "st.sdk", "SparkTech Software Development Kit");
 
             #region FirstInit
             {
@@ -38,11 +75,17 @@
             #endregion
 
             #region Version
-            
+            {
+                SDKMenu.AddSubMenu("Version", "st.sdk.version");
+
+            }
             #endregion
 
             #region License
+            {
+                SDKMenu.AddSubMenu("License", "st.sdk.license");
 
+            }
             #endregion
 
             #region Info
@@ -51,19 +94,12 @@
                 info.AddGroupLabel("Version");
                 var allow = new CheckBox("Perform update checks");
                 info.Add("st.sdk.info.version.check", allow);
-                var version = new Label(allow.CurrentValue ? "Version - Checking..." : "Update checks disabled");
+                var version = new Label(allow.CurrentValue ? "Version - Checking failure!" : "Update checks disabled");
                 info.Add("st.sdk.info.version", version);
                 info.AddSeparator();
                 info.AddGroupLabel("Please report any bugs or suggestions at:");
                 info.AddLabel("Skype: \"wiktorsharp\"");
                 info.AddLabel("Discord: @spark");
-
-                /*
-                info.AddGroupLabel("Donations:");
-                info.AddLabel("paypal.me/Wiciaki");
-                info.AddLabel("Bitcoins or any other methods, just message me"); // TODO
-                info.AddLabel("If you've donated, message me please, so I can thank you for that again :)");
-                */
 
                 if (allow.CurrentValue)
                 {
