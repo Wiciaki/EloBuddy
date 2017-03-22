92b891d406eb2756fa4db729d15e53859eb7bcb7
Wiciaki <wicia_6@wp.pl>
2017-03-22 23:38:59 +0100

    daily version push

--- SparkTech.SDK/Web/Versioning.cs
+++ SparkTech.SDK/Web/Versioning.cs
@@ -2,14 +2,8 @@
 {
     public static class Versioning
     {
-        /// <summary>
-        /// The version for the current assembly
-        /// </summary>
-        public const string VersionSDK = "0.2.1.7";
+        public const string VersionSDK = "0.2.1.8";
 
-        /// <summary>
-        /// The version for the <see cref="E:AllyPingSpammer"/>
-        /// </summary>
         public const string VersionAllyPingSpammer = "2.2.0.2";
     }
 }
